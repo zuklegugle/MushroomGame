2d860f0a053c59bc6647ee16d0de5692eb15a5a1
Kacper <nusbajzel@gmail.com>
2022-03-20 00:16:00 +0100

    change

---
+++ scripts/interaction/interactor.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+class_name Interactor
+
+signal interacted
+signal interactable_detected
+
+var _interactables_in_range : Array
+
+func _on_interactable_enter(body):
+	pass
+
+func _on_interactable_exit(body):
+	pass
